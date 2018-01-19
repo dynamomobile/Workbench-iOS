eeb32c01a6fc39747efec803dfd59b622c93307d
Edward Patel <edward@dyna.mo>
2018-01-19 10:21:18 +0100

    Add hex color parsing

--- Workbench/Dynamo/Extensions/UIColor+Extensions.swift
+++ Workbench/Dynamo/Extensions/UIColor+Extensions.swift
@@ -21,6 +21,21 @@ private let lookup: [String: UIColor] = [
 extension UIColor {
 
     static func colorBy(name: String, defaultColor: UIColor? = .red) -> UIColor? {
+        if let match = name.range(of: "^([0-9a-fA-F]{2}){3,4}$",
+                                  options: .regularExpression,
+                                  range: nil,
+                                  locale: nil),
+            var hex = Int(name, radix: 16) {
+            var alpha: CGFloat = 1.0
+            if name.distance(from: match.lowerBound, to: match.upperBound) == 8 {
+                alpha = CGFloat(hex & 0xff)/255.0
+                hex = hex >> 8
+            }
+            return UIColor(red: CGFloat((hex >> 16) & 0xff)/255.0,
+                           green: CGFloat((hex >> 8) & 0xff)/255.0,
+                           blue: CGFloat(hex & 0xff)/255.0,
+                           alpha: alpha)
+        }
         return lookup[name] ?? defaultColor
     }
 
