e7424eb6ceb34596c9efdf69040ebfaa18f27aa5
Edward Patel <edward@dyna.mo>
2018-02-27 15:38:59 +0100

    Added update from UITextField and UITextView to context

--- Workbench/Dynamo/Context/UIView+Context.swift
+++ Workbench/Dynamo/Context/UIView+Context.swift
@@ -2,10 +2,11 @@ import UIKit
 
 extension UIView {
 
-    static let contextPointerBase      = UnsafeRawPointer("contextPointerBase")
-    static let contextKey              = contextPointerBase+0
-    static let viewControllerKey       = contextPointerBase+1
-    static let parentViewControllerKey = contextPointerBase+2
+    static let contextPointerBase        = UnsafeRawPointer("contextPointerBase")
+    static let contextKey                = contextPointerBase+0
+    static let viewControllerKey         = contextPointerBase+1
+    static let parentViewControllerKey   = contextPointerBase+2
+    static let textChangeNotificationKey = contextPointerBase+3
 
     // Setup childViewController when loaded from property "viewcontroller" and has parentViewcontroller
     func contextCheckToSetup() {
@@ -41,6 +42,38 @@ extension UIView {
                 control.addTarget(self, action: #selector(UISlider.wb_contextSliderUpdate), for: .valueChanged)
                 control.wb_contextSliderUpdate(control)
             }
+            if self.isKind(of: UITextView.self) {
+                let proto = NotificationCenter.default.addObserver(forName: .UITextViewTextDidChange,
+                                                                   object: self,
+                                                                   queue: nil) { (notification) in
+                    if let textView = notification.object as? UITextView,
+                        let parent = textView.superview,
+                        let context = textView.context,
+                        let text = textView.text {
+                        parent.setContextValue(text, forKey: context)
+                    }
+                }
+                objc_setAssociatedObject(self,
+                                         UIView.textChangeNotificationKey,
+                                         proto,
+                                         .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            }
+            if self.isKind(of: UITextField.self) {
+                let proto = NotificationCenter.default.addObserver(forName: .UITextFieldTextDidChange,
+                                                                   object: self,
+                                                                   queue: nil) { (notification) in
+                    if let textField = notification.object as? UITextField,
+                        let parent = textField.superview,
+                        let context = textField.context,
+                        let text = textField.text {
+                        parent.setContextValue(text, forKey: context)
+                    }
+                }
+                objc_setAssociatedObject(self,
+                                         UIView.textChangeNotificationKey,
+                                         proto,
+                                         .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            }
         }
     }
 
@@ -61,6 +94,14 @@ extension UIView {
         if self.isKind(of: UILabel.self), let label = self as? UILabel,
             let contextValue = contextValue(forKey: key) as? ContextTransformer {
             label.text = contextValue.toString(extra: extra)
+        }
+        if self.isKind(of: UITextView.self), let textView = self as? UITextView,
+            let contextValue = contextValue(forKey: key) as? ContextTransformer {
+            textView.text = contextValue.toString(extra: extra)
+        }
+        if self.isKind(of: UITextField.self), let textField = self as? UITextField,
+            let contextValue = contextValue(forKey: key) as? ContextTransformer {
+            textField.text = contextValue.toString(extra: extra)
         }
     }
 
